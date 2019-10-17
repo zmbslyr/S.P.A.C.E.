3b6cad4c6331ee5672c7a0dc3581982381b25e37
Mark Hedgeland <mhedgeland@gmail.com>
2019-10-16 22:12:05 -0400

    [GLOBAL][js] Added background and grid on top of it

--- js/main.js
+++ js/main.js
@@ -12,9 +12,35 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload () {
+  // Loads background image
+  this.load.image('background', 'assets/sprites/background.png');
 }
 
 function create () {
+  // Displays background image
+  this.add.image(400, 300, 'background');
+  // Set up graphics and draw grid on top of background
+  var graphics = this.add.graphics();
+  drawGrid(graphics);
+}
+
+/**
+ * drawGrid - Function to draw grid superficially
+ * @graphics: Graphics obj
+ *
+ * Return: void
+ */
+function drawGrid (graphics) {
+  graphics.lineStyle(1, 0x0000ff, 0.8);
+  for (var index = 0; index < 8; index++) {
+    graphics.moveTo(0, index * 80);
+    graphics.lineTo(800, index * 80);
+  }
+  for (var index2 = 0; index2 < 10; index2++) {
+    graphics.moveTo(index2 * 80, 0);
+    graphics.lineTo(index2 * 80, 600);
+  }
+  graphics.strokePath();
 }
 
 function update () {
