ee9037b81310750dc76a16fd61b1708af2ea2bba
Mark Hedgeland <mhedgeland@gmail.com>
2019-10-17 20:19:33 -0400

    [GLOBAL][js] Now supports enemies moving along path

--- js/main.js
+++ js/main.js
@@ -9,10 +9,13 @@ var config = {
   }
 };
 
+/* Global variables */
 var game = new Phaser.Game(config);
 var path;
 var turrets;
 var enemies;
+var ENEMY_SPEED = 1 / 10000;
+var BULLET_DAMAGE = 50;
 
 // Matrix representing the map
 var map = [
@@ -26,26 +29,74 @@ var map = [
   [0, 0, 0, 0, 0, 0, 0, 0,-1, 0]
 ];
 
+/* Classes */
 // Turret class to handle all turret functions
 var Turret = new Phaser.Class({
   Extends: Phaser.GameObjects.Image,
   initialize:
   function Turret (scene) {
     Phaser.GameObjects.Image.call(this, scene, 0, 0, 'turret1');
+    this.nextTic = 0;
   },
   place: function (i, j) {
     this.y = i * 80 + 80 / 2;
     this.x = j * 80 + 80 / 2;
     map[i][j] = 1;
+  },
+  update: function (time, delta) {
+    if (time > this.nextTic) {
+      this.nextTic = time + 1000;
+    }
   }
 });
 
+// Enemy class to handle all enemy functions
+var Enemy = new Phaser.Class({
+  Extends: Phaser.GameObjects.Image,
+  initialize:
+  function Enemy (scene) {
+    Phaser.GameObjects.Image.call(this, scene, 0, 0, 'enemy1');
+    this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
+    this.hp = 0;
+  },
+
+  startOnPath: function () {
+    this.follower.t = 0;
+    this.hp = 100;
+    path.getPoint(this.follower.t, this.follower.vec);
+    this.setPosition(this.follower.vec.x, this.follower.vec.y);
+  },
+
+  receiveDamage: function (damage) {
+    this.hp -= damage;
+
+    if (this.hp <= 0) {
+      this.setActive(false);
+      this.setVisible(false);
+    }
+  },
+
+  update: function (time, delta) {
+    this.follower.t += ENEMY_SPEED * delta;
+    path.getPoint(this.follower.t, this.follower.vec);
+    this.setPosition(this.follower.vec.x, this.follower.vec.y);
+
+    if (this.follower.t >= 1) {
+      this.setActive(false);
+      this.setVisible(false);
+    }
+  }
+});
+
+// Preload all assets
 function preload () {
   // Loads background image
   this.load.image('background', 'assets/sprites/background.png');
   this.load.image('turret1', 'assets/sprites/turret1.png');
+  this.load.image('enemy1', 'assets/sprites/enemy1.png');
 }
 
+// Create game space
 function create () {
   // Displays background image
   this.add.image(400, 320, 'background');
@@ -61,6 +112,9 @@ function create () {
   path.lineTo(680, 640);
   graphics.lineStyle(3, 0xffffff, 1);
   path.draw(graphics);
+  // Create enemies and spawn them
+  enemies = this.add.group({ classType: Enemy, runChildUpdate: true });
+  this.nextEnemy = 0;
   // Allows you to place turrets on the map
   turrets = this.add.group({ classType: Turret, runChildUpdate: true });
   this.input.on('pointerdown', placeTurret);
@@ -83,9 +137,6 @@ function drawGrid (graphics) {
     graphics.lineTo(index2 * 80, 640);
   }
   graphics.strokePath();
-}
-
-function update () {
 }
 
 /**
@@ -114,6 +165,25 @@ function placeTurret (pointer) {
       turret.setActive(true);
       turret.setVisible(true);
       turret.place(i, j);
+    }
+  }
+}
+
+/**
+ * update - Function to update the game
+ * @time:
+ * @delta:
+ *
+ * Return: void
+ */
+function update (time, delta) {
+  if (time > this.nextEnemy) {
+    var enemy = enemies.get();
+    if (enemy) {
+      enemy.setActive(true);
+      enemy.setVisible(true);
+      enemy.startOnPath();
+      this.nextEnemy = time + 2000;
     }
   }
 }
